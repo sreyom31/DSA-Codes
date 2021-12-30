137b826791b178625aca0cf5e18bcb0eb1fdb9eb
sreyom31 <sreyom31@gmail.com>
2021-12-30 12:03:47 +0530

    recursive and looping approach to calculate taylor series via horner's method
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- 01_section5_Recursion/10_Taylor_Series_Recursion.c
+++ 01_section5_Recursion/10_Taylor_Series_Recursion.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
+
+//! This is the normal approach to calculate taylor series via recursion
+// double e(int x, int n)
+// {
+//     static double p = 1, f = 1;
+//     double r;
+
+//     if (n == 0)
+//         return 1;
+//     r = e(x, n - 1);
+//     p = p * x;
+//     f = f * n;
+//     return r + p / f;
+// }
+
+//! Taylor Series using horner's rule and recursion
+// double e(int x, int n)
+// {
+//     static double s;
+//     if (n == 0)
+//         return s;
+//     s = 1 + x * s / n;
+//     return e(x, n - 1);
+// }
+
+//! Taylor Series using horner's rule and loop
 double e(int x, int n)
 {
-    static double p = 1, f = 1;
-    double r;
+    double s = 1;
+    int i;
+    double num = 1;
+    double den = 1;
 
-    if (n == 0)
-        return 1;
-    r = e(x, n - 1);
-    p = p * x;
-    f = f * n;
-    return r + p / f;
+    for (i = 1; i <= n; i++)
+    {
+        num *= x;
+        den *= i;
+        s += num / den;
+    }
+    return s;
 }
+
 int main()
 {
     printf("%lf \n", e(4, 15));
